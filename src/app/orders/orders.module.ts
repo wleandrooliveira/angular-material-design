e3f36daf21aa83af85dc02afc19754f6ee2753cf
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2020-06-14 04:51:49 -0300

    Add-data-tables and pagintation

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -9,9 +9,13 @@ import { OrderNewComponent } from './order-new/order-new.component';
 // Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
@@ -26,9 +30,13 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
     // Material
     MatButtonModule,
     MatCardModule,
+    MatCheckboxModule,
     MatDatepickerModule,
     MatIconModule,
     MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
     MatTabsModule,
     MatMomentDateModule
   ],
